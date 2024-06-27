3c01f9886eeaaf6ac3ebe583a709c5a9187ab409
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-06-28 00:59:49 +0700

    Update 1.5

--- script/login-page/script.js
+++ script/login-page/script.js
@@ -1,12 +1,21 @@
 const submit_btn = document.querySelector('#btn-kirim');
 
 document.querySelector('#input-query').addEventListener("keydown", function(event) {
+    nama = document.querySelector('#input-query').value;
     if (event.keyCode === 13 || event.key === "Enter") {
         submit_btn.click();
     }
 });
 
-submit_btn.addEventListener('click', () =>{
+submit_btn.addEventListener('click', async () =>{
     nama = document.querySelector('#input-query').value;
-    window.location.href = `./app/index.html?n=${nama}`;
+    if(nama === ''){
+        return await Swal.fire({
+            title: "Opps..",
+            text: "Kamu belum menuliskan nama!",
+            icon: "error"
+        });
+    }else{
+        window.location.href = `./app/index.html?n=${nama}`;
+    }
 })
--- script/script.js
+++ script/script.js
@@ -12,8 +12,29 @@ function ceklogin(){
 };
 ceklogin();
 
-api = 'https://api.nyxs.pw/ai/gemini-advance?text=';
-
+document.querySelector('#logout-btn').addEventListener('click', ()=>{
+    let timerInterval;
+    Swal.fire({
+        title: "Wait....",
+        html: "I will Logout in <b></b> milliseconds.",
+        timer: 2000,
+        timerProgressBar: true,
+    didOpen: () => {
+        Swal.showLoading();
+        const timer = Swal.getPopup().querySelector("b");
+        timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+        }, 100);
+    },
+    willClose: () => {
+        clearInterval(timerInterval);
+    }
+    }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+            window.location.href = './index.html';
+        }
+    });
+})
 document.querySelector('#toggle').addEventListener('change', function(){
     // console.log(this.checked);
     if(!this.checked){
@@ -22,3 +43,45 @@ document.querySelector('#toggle').addEventListener('change', function(){
         return document.querySelector('html').classList.add('dark');
     }
 });
+
+api = 'https://api.nyxs.pw/ai/gemini-advance?text=';
+document.querySelector('#submit-btn').addEventListener('click', ()=>{
+    const query = document.querySelector('#input-query').value;
+    if(query === ''){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Kamu belum menuliskan apapun!",
+        });
+    }else{
+        fetch(`${api}${query}`)
+            .then((response) =>{
+                if (!response.ok){
+                    console.error(response.statusText);
+                };
+                return response.json();
+            }).then(async(Response) =>{
+                // console.log(Response.result);
+                document.querySelector('#main').innerHTML = await fragment(query,Response.result);
+            }
+        )
+        document.querySelector('#main').innerHTML = loading_fragment();
+        query.value = ''
+    }
+})
+
+function fragment(ask,answer){
+    return `<div class="chat chat-start">
+                <div class="chat-bubble">
+                    ${ask}
+                </div>
+            </div>
+            <div class="chat chat-end">
+                <div class="chat-bubble">
+                    ${answer}
+                </div>
+            </div>`
+}
+function loading_fragment(){
+    return `<span class="loading loading-dots loading-lg text-center mx-auto dark:text-white"></span>`;
+}
